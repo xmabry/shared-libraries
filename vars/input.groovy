98495b722a5de29efe9e30effbd7ca75417dfa51
Kurt Madel <kmadel@mac.com>
2018-01-04 20:58:29 -0500

    Create input.groovy

---
+++ vars/input.groovy
@@ -0,0 +1,14 @@
+def call(Map params = [:]) {
+    String message = params.message
+    String ok = params.get('ok', null)
+
+    timeout(time: 20, unit: SECONDS) {
+        /* invoke the built-in input step */
+        return steps.input(message: message,
+          ok: ok)
+    }
+}
+/* Convenience overload */
+def call(String message) {
+    return call(message: message)
+}
