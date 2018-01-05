29b9206c8ef8d41daab57f58a7d3f9f6dac541e9
Kurt Madel <kmadel@mac.com>
2018-01-04 21:20:05 -0500

    Update input.groovy

--- vars/input.groovy
+++ vars/input.groovy
@@ -2,7 +2,7 @@ def call(Map params = [:]) {
     String message = params.message
     String ok = params.get('ok', null)
 
-    timeout(time: 20, unit: SECONDS) {
+    timeout(time: 20, unit: 'SECONDS') {
         /* invoke the built-in input step */
         return steps.input(message: message,
           ok: ok)
